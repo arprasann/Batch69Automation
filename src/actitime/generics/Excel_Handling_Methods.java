c31328c726b646abadb16813738a22e885f86c77
prasanna <prasanna.ar417@gmail.com>
2024-09-07 15:12:13 +0530

    added CSV methods

---
+++ src/actitime/generics/Excel_Handling_Methods.java
@@ -0,0 +1,44 @@
+package actitime.generics;
+
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class Excel_Handling_Methods {
+	//Count Total rows particular sheet
+		public static int getRowCount(String path,String sheet)
+		{
+	int rc=0;
+	try
+	{
+		FileInputStream fis=new FileInputStream(path);
+		Workbook wb = WorkbookFactory.create(fis);
+		       rc=wb.getSheet(sheet).getLastRowNum();
+	}
+	catch(Exception e)
+	{
+			
+		}
+	return rc;
+	}
+		
+	//-----------------------------------------------------------------
+		//Get the Cell particular Row
+		public static String getCellValue(String path,String sheet,int r,int c)
+		{
+			String v=" ";
+			try
+			{
+				FileInputStream fis=new FileInputStream(path);
+				 Workbook wb = WorkbookFactory.create(fis);
+				 v=wb.getSheet(sheet).getRow(r).getCell(c).toString();
+			}
+			catch(Exception e)
+			{
+				
+			}
+			return v;
+		}
+
+}
